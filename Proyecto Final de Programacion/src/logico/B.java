e860c178c9015f3d2f6ad07d7239d011237af431
gilbe <gilbe@Gagster01>
2022-07-04 22:31:56 -0400

    creando clase b

--- Proyecto Final de Programacion/src/logico/B.java
+++ Proyecto Final de Programacion/src/logico/B.java
@@ -2,8 +2,27 @@ package logico;
 
 public class B {
 
-	public B() {
-		// TODO Auto-generated constructor stub
+	private int a;
+	private int b;
+	
+	public B(int a, int b) {
+		super();
+		this.a = a;
+		this.b = b;
 	}
+	
+	public int getA() {
+		return a;
+	}
+	public void setA(int a) {
+		this.a = a;
+	}
+	public int getB() {
+		return b;
+	}
+	public void setB(int b) {
+		this.b = b;
+	}
+	
 
 }
